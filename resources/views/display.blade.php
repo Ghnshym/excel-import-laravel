f8cd16a13bc948be2957b9f0e6e1d1d5eee9d063
Ghanshyam kumar <bca1902113@gmail.com>
2023-09-22 12:04:50 +0530

    fifth

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -1,9 +1,39 @@
-<!-- resources/views/display.blade.php -->
-<table>
-    <thead>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Page</title>
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Excel Import</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<table class="table table-bordered table-striped">
+    <thead class="thead-dark">
         <tr>
             @foreach($headers as $header)
-                <th>{{ $header }}</th>
+                @if (!empty(trim($header))) <!-- Check if the header is not empty or contains only whitespace -->
+                    <th>{{ $header }}</th>
+                @endif
             @endforeach
         </tr>
     </thead>
@@ -11,9 +41,22 @@
         @foreach($data as $row)
             <tr>
                 @foreach($row as $cell)
-                    <td>{{ $cell }}</td>
+                    @if (!empty(trim($cell))) <!-- Check if the cell is not empty or contains only whitespace -->
+                        <td>{{ $cell }}</td>
+                    @endif
                 @endforeach
             </tr>
         @endforeach
     </tbody>
 </table>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+<!-- resources/views/display.blade.php -->
