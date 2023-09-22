6fefe0d36ff0319856cbd45e4b8d54fcb7258fde
ghanshyam <bca1902113@gmail.com>
2023-09-22 17:51:25 +0530

    bug fixed for trimed empty row or column

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -17,49 +17,70 @@ class ExcelController extends Controller
     }
 
     public function import(Request $request)
-{
-    // Validate the uploaded file
-    $request->validate([
-        'file' => 'required|mimes:xlsx,xls',
-    ]);
-
-    // Generate a unique table
-    $tableName = 'dynamic_table_' . now()->format('YmdHis');
-
-    // Store the uploaded file
-    $file = $request->file('file');
-    $filePath = Storage::putFile('uploads', $file);
-
-    $data = Excel::toArray([], $filePath);
-
-    $dataRows = array_slice($data[0], 1);
-
-    $headers = $data[0][0];
-    $validatedHeaders = [];
-    foreach ($headers as $header) {
-        $sanitizedHeader = preg_replace('/[^a-zA-Z0-9_]/', '_', trim($header));
-        $finalHeader = !empty($sanitizedHeader) ? $sanitizedHeader : 'default_column_' . Str::random(8);
-        $validatedHeaders[] = $finalHeader;
-    }
-
-    // Use Laravel's Schema Builder to dynamically create a new table
-    Schema::create($tableName, function ($table) use ($validatedHeaders) {
-        $table->id();
-        foreach ($validatedHeaders as $header) {
-            $table->string($header)->nullable();
+    {
+        // Validate the uploaded file
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls',
+        ]);
+    
+        // Generate a unique table
+        $tableName = 'dynamic_table_' . now()->format('YmdHis');
+    
+        // Store the uploaded file
+        $file = $request->file('file');
+        $filePath = Storage::putFile('uploads', $file);
+    
+        $data = Excel::toArray([], $filePath);
+    
+        // Remove empty rows (those with all empty cells)
+        $dataRows = array_filter(array_slice($data[0], 1), function ($row) {
+            return !empty(array_filter($row, 'strlen'));
+        });
+    
+        if (empty($dataRows)) {
+            // Handle the case where there is no data to import
+            return redirect()->back()->with('error', 'No data found in the uploaded Excel file.');
         }
-        $table->timestamps();
-    });
-
-    foreach ($dataRows as $row) {
-        DB::table($tableName)->insert(array_combine($validatedHeaders, $row));
+    
+        // Get the first row as headers
+        $headers = $data[0][0];
+    
+        // Filter out empty headers
+        $nonEmptyHeaders = array_filter($headers, 'strlen');
+    
+        if (empty($nonEmptyHeaders)) {
+            // Handle the case where all headers are empty
+            return redirect()->back()->with('error', 'No valid column headers found in the uploaded Excel file.');
+        }
+    
+        $validatedHeaders = [];
+        foreach ($nonEmptyHeaders as $header) {
+            $sanitizedHeader = preg_replace('/[^a-zA-Z0-9_]/', '_', trim($header));
+            $finalHeader = !empty($sanitizedHeader) ? $sanitizedHeader : 'default_column_' . Str::random(8);
+            $validatedHeaders[] = $finalHeader;
+        }
+    
+        // Use Laravel's Schema Builder to dynamically create a new table
+        Schema::create($tableName, function ($table) use ($validatedHeaders) {
+            $table->id();
+            foreach ($validatedHeaders as $header) {
+                $table->string($header)->nullable();
+            }
+            $table->timestamps();
+        });
+    
+        foreach ($dataRows as $row) {
+            // Filter out empty cells in the row
+            $nonEmptyRow = array_filter($row, 'strlen');
+            DB::table($tableName)->insert(array_combine($validatedHeaders, $nonEmptyRow));
+        }
+    
+        return view('display', [
+            'data' => $dataRows, 
+            'headers' => $nonEmptyHeaders, 
+            'tableName' => $tableName,
+        ]);
     }
-
-    return view('display', [
-        'data' => $dataRows, 
-        'headers' => $headers, 
-        'tableName' => $tableName,
-    ]);
-}
+    
 
 }
